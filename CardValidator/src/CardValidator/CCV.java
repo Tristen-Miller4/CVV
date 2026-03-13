6a1f6c98f59e5964b541803ed4e99916e65b1bb3
Summi <Summi@DESKTOP-6UC4OQQ>
2026-03-12 23:42:20 -0600

    Added Comments to the test card method and the main method

--- CardValidator/src/CardValidator/CCV.java
+++ CardValidator/src/CardValidator/CCV.java
@@ -24,7 +24,7 @@ public class CCV {
 		//cardLoop();
 		 Scanner userIntInput = new Scanner(System.in);
 		
-		
+		//This is the main menu that asks if you want to validate a card
 		System.out.println("Welcome to the Card Validator");
 		System.out.println("Press 1) if you would like to validate an inputed card. Press 2) if you want to check the text file");
       int userChoice = userIntInput.nextInt();
@@ -82,25 +82,32 @@ public class CCV {
 
 	public static boolean testCard(Long cardNumber) throws IOException {
 		 String number = Long.toString(cardNumber);
-	        int sum = 0;
-	        boolean doubleDigit = false;
+	        int sum = 0;//This sets the value of the 16 digit cards to 0
+	        boolean doubleDigit = false; //sets the first value as false because you are supposed to double every second digit that you move from
+	        
 
 	        
-	        for (int i = number.length() - 1; i >= 0; i--) {
-	            int digit = number.charAt(i) - '0';
+	        for (int i = number.length() - 1; i >= 0; i--) // this is running the for loop through the card digits from right to left instead of right to left. It goes to the end value minus 1 to get the last digit and then goes from there.
+	        {
+	            int digit = number.charAt(i) - '0';// this is converting the string digit that is given in the card value to an actual integer 
+	            // so this finds what ever number it is going through (i) and it takes its character and converts it to its ASCII number value and then subtracts it by '0' which is 48 to get its final value
+	            // this allows the system to do math and convert the characters to integers.
 
-	            if (doubleDigit) {
-	                digit = digit * 2;
-	                if (digit > 9) {
-	                    digit = digit - 9;
+	            if (doubleDigit) //this is when the digit should be doubled
+	            {
+	                digit = digit * 2;// this is when the digit gets multiplied by 2 
+	                if (digit > 9) // this is asking is the digit that was doubled bigger than 9 and goes into a double digit number?
+	                
+	                {
+	                    digit = digit - 9;// if the number is bigger than 9 then it subtracts 9 to get it back to a single digit number
 	                }
 	            }
 
-	            sum += digit;
-	            doubleDigit = !doubleDigit;
+	            sum += digit; // this adds the values of the card that has been either doubled and numbers that have been doubled and subtracted by 9  and then stores the results back into sum
+	            doubleDigit = !doubleDigit; // this means for the every other digit that isnt the second it doesnt double it. ! means NOT
 	        }
 
-	        return sum % 10 == 0;
+	        return sum % 10 == 0; // this makes it so if the result of sum is divisible by 10 then the card is valid
 	    }
 
 
